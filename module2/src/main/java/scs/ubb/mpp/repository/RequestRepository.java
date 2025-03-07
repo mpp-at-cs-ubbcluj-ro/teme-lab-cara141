c228225f6450cba3bed89faeb81a24b86d0e97e8
cara141 <102748909+cara141@users.noreply.github.com>
2025-03-07 13:42:27 +0200

    Lab 2 - finalizat

---
+++ module2/src/main/java/scs/ubb/mpp/repository/RequestRepository.java
@@ -0,0 +1,7 @@
+package scs.ubb.mpp.repository;
+
+import scs.ubb.mpp.model.ComputerRepairRequest;
+
+public class RequestRepository extends AbstractRepository<ComputerRepairRequest, Integer>{
+    public RequestRepository(){}
+}
